f68b8027b158101d7c85ab527ff29c24d60ec8e9
damienbod <damien_bod@hotmail.com>
2022-11-15 20:16:35 +0100

    Code clean up

--- AadMultiApis/MultiAADWebApi/Startup.cs
+++ AadMultiApis/MultiAADWebApi/Startup.cs
@@ -14,7 +14,6 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-
         services.AddAuthentication("multitenantapi")
             .AddMicrosoftIdentityWebApi(Configuration, "AzureADMultiApi", "multitenantapi");
 
