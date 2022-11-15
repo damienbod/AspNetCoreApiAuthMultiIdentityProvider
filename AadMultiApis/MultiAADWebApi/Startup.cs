34b5c27f3c9527a69401e3ef2e1175b4f0519cee
damienbod <damien_bod@hotmail.com>
2022-11-15 16:39:27 +0100

    Multi test

---
+++ AadMultiApis/MultiAADWebApi/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Net.Http.Headers;
+using OpenIddict.Validation.AspNetCore;
+
+namespace WebApi;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddAuthentication("multitenantapi")
+            .AddMicrosoftIdentityWebApi(Configuration, "AzureADMultiApi", "multitenantapi");
+
+        services.AddAuthentication("singletenantapi")
+            .AddMicrosoftIdentityWebApi(Configuration, "AzureADSingleApi", "singletenantapi");
+
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        IdentityModelEventSource.ShowPII = true;
+        //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers().RequireAuthorization();
+        });
+    }
+}
