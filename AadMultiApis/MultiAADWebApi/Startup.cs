5cc2072aaa759972fce581f0813b7db36b766a6d
damienbod <damien_bod@hotmail.com>
2022-11-19 08:39:18 +0100

    Update packages

--- AadMultiApis/MultiAADWebApi/Startup.cs
+++ AadMultiApis/MultiAADWebApi/Startup.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Logging;
